a2e7c40288f573e4119ac1c1a5522ff8079c7dd9
ji sun lee <ellijs@hotmail.com>
2021-03-22 03:18:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,7 +79,7 @@ class TicTacToe
     WIN_COMBINATIONS.detect do |combo|
       @board[combo[0]] == @board[combo[1]] &&
       @board[combo[1]] == @board[combo[2]] &&
-      position_taken?
+      position_taken?(combo[0])
     end
   end
 
